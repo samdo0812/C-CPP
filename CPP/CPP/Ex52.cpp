a25bb9bff29d49708ad2ce8064beafb52e57c950
samdo0812 <39455589+samdo0812@users.noreply.github.com>
2019-01-19 20:12:40 +0900

    const & define & enum
    const & define & enum

---
+++ CPP/CPP/Ex52.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define PI 3.14159	//define을 매크로라고 부름
+
+
+//const , 매크로
+
+int main() {
+	//const float PI = 3.14159;
+
+	float a = PI;
+	
+	printf("π = %.2f\n" , PI);
+	//printf("&π = %d\n", &PI);
+
+}
---
+++ CPP/CPP/Ex53.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+//enum 
+enum EGameState
+{
+
+	GAMESTATE_MAINMENU,
+	GAMESTATE_PLAYING,
+	GAMESTATE_PAUSED,
+	GAMESTATE_GAMEOVER
+
+};
+
+int main() {
+
+	int currState = GAMESTATE_MAINMENU;
+	while (true)
+	{
+		switch (currState)
+		{
+		case GAMESTATE_MAINMENU:
+			break;
+
+		case GAMESTATE_PLAYING:
+			break;
+
+		case GAMESTATE_PAUSED:
+			break;
+
+		case GAMESTATE_GAMEOVER:
+			break;
+
+		}
+	}
+}

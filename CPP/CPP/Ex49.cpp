c2c4af089c4f2911c74c239eb2ff34bb19d46b1b
samdo0812 <39455589+samdo0812@users.noreply.github.com>
2019-01-19 18:58:17 +0900

    typedef & array pointer
    typedef & array pointer

---
+++ CPP/CPP/Ex49.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+typedef int Point[2];
+typedef Point *PointPtr;
+
+int main() {
+	Point p = { 3,4 };	//int p[2] {3,4};
+	PointPtr pp = &p;	//Point *pp = &p;
+
+	printf("%d %d %d\n", **pp, (*pp)[0], (*pp)[1]);
+	//**pp = *p = p[0]
+}

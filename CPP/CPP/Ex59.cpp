a61cfeec60959a7532b23e74cfbdaa5638119597
samdo0812 <39455589+samdo0812@users.noreply.github.com>
2019-01-20 18:57:34 +0900

    Bitwise shift operation
    Bitwise shift operation

--- CPP/CPP/Ex59.cpp
+++ CPP/CPP/Ex59.cpp
@@ -1,6 +1,16 @@
 #include <stdio.h>
 // 시프트 연산
-
+// << >> 
 int main() {
+	char a = 22;	//char -128 ~ 127
+
+	printf("%d\n", a << 1 );
+	printf("%d\n", a << 3 );
+	printf("%d\n", a << 6 );
+	
+
+	printf("%d\n", a >> 1);
+	printf("%d\n", a >> 3);
+	printf("%d\n", a >> 6);
 
 }

9a835a0dade5c7cd995bd82b5a4421fefca0d98e
samdo0812 <39455589+samdo0812@users.noreply.github.com>
2019-01-21 19:17:20 +0900

    exit
    exit

---
+++ CPP/CPP/Ex66.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	int sum = 0;
+
+	for (int i = 0; i < 4; i++)
+	{
+		int n;
+
+		scanf("%d", &n);
+
+		if (n <= 0)
+		{
+			printf("INPUT ERROR\n");
+			exit(0);
+		}
+		
+		sum += n;
+	}
+
+	printf("%d\n", sum);
+}

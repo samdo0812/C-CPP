11e4584a8153d792dbfd5e7b5475919cae19b116
samdo0812 <39455589+samdo0812@users.noreply.github.com>
2019-01-22 20:01:27 +0900

    override
    override

---
+++ CPP/CPP/Ex73.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	//r-value , 수정 가능한
+
+	int a(5);
+	int &r1 = a;
+	//int &r2 = 3; 3은 상수라 수정 x
+	//int &r3 = a * a;
+	
+	//int &&r1 = a;  a는 수정 가능하기 때문에 x
+	int &&r2 = 3;
+	int &&r3 = a * a;
+
+}

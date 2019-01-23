ed37df1d71bb437ed53d23507b648af5cf6281b1
samdo0812 <39455589+samdo0812@users.noreply.github.com>
2019-01-23 14:59:45 +0900

    score
    score

---
+++ CPP/CPP/Ex76.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string name;
+	int score;
+
+	cout << "name : ";
+	cin >> name;
+	cout << "score : ";
+	cin >> score;
+
+	cout << name << "'s score : " << score << endl;
+}

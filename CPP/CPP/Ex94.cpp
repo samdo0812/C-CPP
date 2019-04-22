c24b5c77c28f308e22d054fd69fb93ca8aa4fdab
samdo0812 <39455589+samdo0812@users.noreply.github.com>
2019-04-22 16:55:51 +0900

    Vector
    Vector

--- CPP/CPP/Ex94.cpp
+++ CPP/CPP/Ex94.cpp
@@ -13,17 +13,32 @@ public:
 
 	float GetX() const;
 	float GetY() const;
+	
+	static Vector2 Sum(Vector2 a, Vector2 b)
+	{
+		return Vector2(a.x + b.x, a.y + b.y);
+	}
+
+	Vector2 Add(Vector2 rhs)
+	{
+		return Vector2(x + rhs.x, y + rhs.y);
+	}
 private:
 	float x;
 	float y;
 };
 
+
 int main() {
 	Vector2 a(2, 3);
 	Vector2 b(-1, 4);
+	Vector2 c1 = Vector2::Sum(a, b);
+	Vector2 c2 = a.Add(b);
 
 	cout << a.GetX() << ", " << a.GetY() << endl;
 	cout << b.GetX() << ", " << b.GetY() << endl;
+	cout << c1.GetX() << ", " << c1.GetY() << endl;
+	cout << c2.GetX() << ", " << c2.GetY() << endl;
 
 }
 
